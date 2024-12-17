43fb8ab5e2d258052ef5c23da382e2853d8fe7ec
Your Name <your_email@example.com>
2024-12-17 17:14:45 +0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     images: {
-      domains: ['yourdomain.com'], 
+      loader: 'default',
+      path: '/',
     },
   };
   
